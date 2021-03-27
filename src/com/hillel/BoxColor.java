481a91cc26bfc89ada3d1d978524f7fec3a17ebd
DDymanskiy <dymanskidv@gmail.com>
2021-03-27 10:23:13 +0200

    1. Убрали цвет из класса Box; 2. Добавили новый класс BoxColor. В нем унаследовали все свойства от родительского Box свойства и конструктор, добавили цвет в конструктор дочернего класса. 3.Создал enum с цветами и материалами. 4.В Main убрал запрос из консоли получения материала, добавил в создание материалы из перечислений.

---
+++ src/com/hillel/BoxColor.java
@@ -0,0 +1,18 @@
+package com.hillel;
+
+public class BoxColor extends Box {
+    private String color;
+
+    public BoxColor(int length, int width, int height, String fabric, String color) {
+        super(length, width, height, fabric);//конструктор родительского класса
+        this.color = color;//определяем цвет коробки
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
